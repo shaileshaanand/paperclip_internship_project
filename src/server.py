843c37e2bd2ae2db302e19c3e95a7a9b9afd1089
Shailesh Aanand <anaandshailu@gmail.com>
2020-12-29 19:47:02 +0530

    changes for heroku

--- src/server.py
+++ src/server.py
@@ -5,33 +5,6 @@ from trendline_scraper import scrape
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    return("""
-    <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Trendline Scraper</title>
-</head>
-
-<body>
-    <h1>Trendline Scraper</h1>
-    <p>
-        Available Endpoints:
-    <ul>
-        <li><a href="api/all">/api/all</a></li>
-        <li><a href="api/bonus_splitl">/api/bonus_split</a></li>
-        <li><a href="api/dividend">/api/dividend</a></li>
-        <li><a href="api/board_meetings">board_meetings</a></li>
-        <li><a href="api/results">api/results</a></li>
-    </ul>
-    </p>
-</body>
-
-</html>""")
-
-
 @app.route('/api/all')
 def get_all():
     return jsonify(scrape('https://trendlyne.com/equity/calendar/all/all/'))
@@ -57,5 +30,36 @@ def get_results():
     return jsonify(scrape('https://trendlyne.com/equity/calendar/all/all/?corporate_actions=Results'))
 
 
-if __name__ == "__main__":
+@app.route('/')
+def root():
+    return("""
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Trendline Scraper</title>
+</head>
+
+<body>
+    <h1>Trendline Scraper API</h1>
+    <p>
+        Available Endpoints:
+    <ul>
+        <li><a href="api/all">/api/all</a></li>
+        <li><a href="api/bonus_split">/api/bonus_split</a></li>
+        <li><a href="api/dividend">/api/dividend</a></li>
+        <li><a href="api/board_meetings">/api/board_meetings</a></li>
+        <li><a href="api/results">/api/results</a></li>
+    </ul>
+    </p>
+</body>
+
+</html>""")
+
+
+def main():
     app.run(threaded=True, port=5000)
+
+
+if __name__ == "__main__":
+    main()
